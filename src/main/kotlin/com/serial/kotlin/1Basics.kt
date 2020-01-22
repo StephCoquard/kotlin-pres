174417db59d951d4a3e52919b958fc1260698a77
scoquard <18576190+StephCoquard@users.noreply.github.com>
2020-01-22 14:49:35 +0100

    Basic and Functions modifs

--- src/main/kotlin/com/serial/kotlin/1Basics.kt
+++ src/main/kotlin/com/serial/kotlin/1Basics.kt
@@ -2,10 +2,11 @@ package com.serial.kotlin
 
 // Created by JetBrain / Opensource / Java interoperability
 
+// No semicolon
+
 // Statically typed / val, var and const (val encouraged)
 var variable = 10
 val value = 20
 
 // Non nullable by default. Nullable variables must be declared accordingly
 var test: String = "value"
-
--- src/main/kotlin/com/serial/kotlin/2Functions.kt
+++ src/main/kotlin/com/serial/kotlin/2Functions.kt
@@ -1,26 +1,14 @@
 package com.serial.kotlin
 
-import java.lang.IllegalArgumentException
-
+// Functions are declared with the "fun" keyword
 // Function main (arguments optionnels)
 
 
 // Function outside class possible
 
 
-// No semi-colon
-
-
 // 'if' and 'switch/when' are expressions
 
-fun calculate(a: Int, b: Int, operation: Operation): Int {
-    return when (operation) {
-        Operation.ADDITION -> a + b
-        Operation.SUBTRACTION -> a - b
-        Operation.MULTIPLICATION -> a * b
-        Operation.DIVISION -> a / b
-    }
-}
 
 // Type inference (only with functions with no body)
 
